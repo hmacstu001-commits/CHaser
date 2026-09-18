e2d607bff5d6fb717926c6021ab0d34c07d456ec
hmacstu001-commits <hmac.stu001@gmail.com>
2026-09-18 09:35:33 +0000

    20260911

---
+++ sample copy 2.py
@@ -0,0 +1,43 @@
+from client.ChaserClient import ChaserClient
+
+direction = "right"
+
+def 次の方向(direction):
+    order = ["right","up","left","down"]
+    i = order. index(direction)
+    return order[(i + 1) % len (order)]
+
+def main():
+    client = ChaserClient("192.168.xx.x", 2009, "test")
+    client.connect()
+
+    while True:
+        # 制御コードとマップ情報を得る
+        control_code, map_info = client.receive()
+        # 制御コードが'0'の場合ループを抜ける
+        if control_code == '0':
+            break
+
+        #今の方向を search する
+        client.get_ready()
+        if direction == "right":
+            control_code, map_info = client.search_right()
+        elif direction == "up":
+            control_code, map_info = client.search_up()
+        # left, down も同様にする
+        client.turn_end()
+
+        if 進める(map_info):
+            client.get_ready()
+            if direction == "right":
+                client.walk_right()
+            elif direction == "up":
+                client.walk_up()
+            client.turn_end()
+        else:
+            direction = 次の方向(direction)     # 右回りに切り替える
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
